7ec724719f6895d336505b288c7054ba4db357b0
Jesus Sainz <JesusSainz@MacBook-Pro-de-Jesus.local>
2014-01-16 20:44:46 -0600

    Initial Commit

---
+++ Ejemplo IOS/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Ejemplo IOS
+//
+//  Created by Jesus Sainz on 16/01/14.
+//  Copyright (c) 2014 SmartPlace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Ejemplo IOSTests/Ejemplo_IOSTests.m
@@ -0,0 +1,34 @@
+//
+//  Ejemplo_IOSTests.m
+//  Ejemplo IOSTests
+//
+//  Created by Jesus Sainz on 16/01/14.
+//  Copyright (c) 2014 SmartPlace. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Ejemplo_IOSTests : XCTestCase
+
+@end
+
+@implementation Ejemplo_IOSTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
